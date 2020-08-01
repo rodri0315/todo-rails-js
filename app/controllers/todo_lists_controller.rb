90b0147efa16e6726ae179ea1b90948ed0e09c94
rodri0315 <rodri0315@gmail.com>
2020-08-01 16:50:35 -0400

    Add todo items and migrations

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class TodoListsController < ApplicationController
-  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, only: %i[show edit update destroy]
 
   # GET /todo_lists
   # GET /todo_lists.json
@@ -9,8 +11,7 @@ class TodoListsController < ApplicationController
 
   # GET /todo_lists/1
   # GET /todo_lists/1.json
-  def show
-  end
+  def show; end
 
   # GET /todo_lists/new
   def new
@@ -18,8 +19,7 @@ class TodoListsController < ApplicationController
   end
 
   # GET /todo_lists/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /todo_lists
   # POST /todo_lists.json
@@ -62,13 +62,14 @@ class TodoListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo_list
-      @todo_list = TodoList.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def todo_list_params
-      params.require(:todo_list).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def todo_list_params
+    params.require(:todo_list).permit(:title)
+  end
 end
--- db/migrate/20200731211308_create_todo_lists.rb
+++ db/migrate/20200731211308_create_todo_lists.rb
@@ -2,7 +2,6 @@ class CreateTodoLists < ActiveRecord::Migration[6.0]
   def change
     create_table :todo_lists do |t|
       t.string :title
-      t.string :body
 
       t.timestamps
     end
