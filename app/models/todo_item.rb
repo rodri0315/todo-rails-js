ba33f884c540a5e0e881af292827901a08c6ea0a
rodri0315 <rodri0315@gmail.com>
2020-08-18 00:42:16 -0400

    Add methods to check completed items, add input.

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+  scope :completed, lambda {
+    where(completed: true)
+  }
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
 class TodoList < ApplicationRecord
   has_many :todo_items
+
+  def percent_complete
+    return 0 if total_items.zero?
+
+    ((100 * completed_items.to_f) / total_items).round(1)
+  end
+
+  def completed_items
+    @completed_items ||= todo_items.completed.count
+  end
+
+  def total_items
+    @total_items ||= todo_items.count
+  end
 end
